879b1996c1789d6387039ad2b738905afa29664b
Wes Dean <wesley-dean@users.noreply.github.com>
2026-09-14 21:53:40 -0400

    test: define module member translation

---
+++ tests/expected/module-members.cpp
@@ -0,0 +1,12 @@
+/**
+ * @ingroup networking
+ * @brief Downloads a resource.
+ */
+int download();
+/**
+ * @ingroup networking
+ * @var NETWORK_TIMEOUT
+ * @brief Default network timeout in seconds.
+ * @details Bash variable: readonly string
+ */
+ReadonlyString NETWORK_TIMEOUT;
