8d4b86117a86b47548b5922c8fb7c9a27e3a184b
Wes Dean <wesley-dean@users.noreply.github.com>
2026-09-14 21:08:59 -0400

    test: cover parameter direction qualifiers

---
+++ tests/fixtures/param-directions.bash
@@ -0,0 +1,11 @@
+## @fn directed()
+## @brief Exercises Doxygen parameter direction qualifiers.
+## @param[in] --config Input configuration path.
+## @param[out] config Output configuration path.
+## @param[in,out] 9value Numeric-leading value updated in place.
+## @param[in] ...items Variadic-style input values.
+## @param[out] values[] Array-style output values.
+## @param[in,out] key= Assignment-style value updated in place.
+directed() {
+    :
+}
